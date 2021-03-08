36609e2be1685fc150e63f7a7b961a3be7167191
Matthew Sykes <matthew.sykes@gmail.com>
2021-03-08 20:24:43 +0000

    Fix ulimit.zsh

--- .oh-my-zsh-custom/ulimit.zsh
+++ .oh-my-zsh-custom/ulimit.zsh
@@ -1,6 +1,6 @@
 # vi: set ft=zsh:
 
-if [ "$(ulimit -Hn)" == "unlimited" ] || (( $(ulimit -Hn) > 8192)) ; then
+if [[ "$(ulimit -Hn)" == "unlimited" ]] || (( $(ulimit -Hn) > 8192 )) ; then
   ulimit -Sn 8192 || true
 else
   ulimit -Sn "$(ulimit -Hn)" || true
