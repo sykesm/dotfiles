68c0819ac3d2f4aad8e3828a9d57fc6402a8528a
Matthew Sykes <matthew.sykes@gmail.com>
2021-05-10 19:05:57 -0400

    Add check for LC_TERMINAL as well as TERM_PROGRAM

--- .bash_it_custom/colorterm.bash
+++ .bash_it_custom/colorterm.bash
@@ -4,6 +4,8 @@
 if [[ -z "$COLORTERM" ]]; then
   if [[ "$TERM_PROGRAM" == *"iTerm"* ]] || [[ "$TERM_PROGRAM" == "alacritty" ]]; then
     export COLORTERM="truecolor"
+  elif [[ "$LC_TERMINAL" == *"iTerm"* ]] || [[ "$LC_TERMINAL" == "alacritty" ]]; then
+    export COLORTERM="truecolor"
   elif [[ -n "$WT_SESSION" ]]; then
     export COLORTERM="truecolor"
   fi
