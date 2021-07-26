e2b121884d0497480ed41e8e05de4a8bba429049
Matthew Sykes <matthew.sykes@gmail.com>
2021-07-25 23:10:17 -0400

    colorterm: enable truecolor for Blink

--- .bash_it_custom/colorterm.bash
+++ .bash_it_custom/colorterm.bash
@@ -1,10 +1,11 @@
 # shellcheck shell=bash
 # vi: set ft=sh:
 
+LC_TERMINAL=${LC_TERMINAL:-$TERM_PROGRAM}
+LC_TERMINAL=${LC_TERMINAL:-$LC_TERMINAL_PROGRAM}
+
 if [[ -z "$COLORTERM" ]]; then
-  if [[ "$TERM_PROGRAM" == *"iTerm"* ]] || [[ "$TERM_PROGRAM" == "alacritty" ]]; then
-    export COLORTERM="truecolor"
-  elif [[ "$LC_TERMINAL" == *"iTerm"* ]] || [[ "$LC_TERMINAL" == "alacritty" ]]; then
+  if [[ "$LC_TERMINAL" == *"iTerm"* ]] || [[ "$LC_TERMINAL" == "alacritty" ]] || [[ "$LC_TERMINAL" == "Blink" ]]; then
     export COLORTERM="truecolor"
   elif [[ -n "$WT_SESSION" ]]; then
     export COLORTERM="truecolor"
