79dd1afce0ec3f596f84f7a41c57bb0bf413494b
Matthew Sykes <matthew.sykes@gmail.com>
2023-02-19 16:08:35 -0500

    nvim: make lualine look more like airline

--- .config/nvim/lua/sykesm/plugins/lualine.lua
+++ .config/nvim/lua/sykesm/plugins/lualine.lua
@@ -5,11 +5,36 @@ if not lualine_ok then
   return
 end
 
+local symbols = {
+  colnr = '\u{e0a3}:',
+  linenr = '\u{e0a1}:',
+  maxlinenr = '\u{2630} ',
+}
+
+local function progress_location()
+  local line = vim.fn.line('.')
+  local col = vim.fn.virtcol('.')
+  local total = vim.fn.line('$')
+  local percent = math.floor(line / total * 100) .. '%%'
+  return string.format('%s %s%d/%d%s%s%d', percent, symbols.linenr, line, total, symbols.maxlinenr, symbols.colnr, col)
+end
+
+local function encoding_fileformat()
+  local fe = vim.opt.fileencoding:get()
+  local ff = vim.bo.fileformat
+  return string.format('%s[%s]', fe, ff)
+end
+
 lualine.setup({
   options = {
     disabled_filetypes = {},
     refresh = {
       statusline = 200,
     },
+  },
+  sections = {
+    lualine_x = { 'filetype', encoding_fileformat },
+    lualine_y = {},
+    lualine_z = { progress_location },
   },
 })
