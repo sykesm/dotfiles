80f6fa4572df95089c57942106668d545b6e158e
Matthew Sykes <matthew.sykes@gmail.com>
2023-03-04 12:20:24 -0500

    nvim: use enable/disable diagnostic command

--- .config/nvim/lua/sykesm/plugins/lsp/diagnostics.lua
+++ .config/nvim/lua/sykesm/plugins/lsp/diagnostics.lua
@@ -7,23 +7,14 @@ for type, icon in pairs(signs) do
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 end
 
--- https://np.reddit.com/r/backtickbot/comments/ng3qz4/httpsnpredditcomrneovimcommentsng0dj0lsp/
 vim.g.lsp_diagnostics_active = true
 function _G.lsp_toggle_diagnostics()
   if vim.g.lsp_diagnostics_active then
     vim.g.lsp_diagnostics_active = false
-    vim.diagnostic.hide()
-    ---@diagnostic disable-next-line: duplicate-set-field
-    vim.lsp.handlers['textDocument/publishDiagnostics'] = function() end
+    vim.diagnostic.disable()
   else
     vim.g.lsp_diagnostics_active = true
-    vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-      virtual_text = true,
-      signs = true,
-      underline = true,
-      update_in_insert = false,
-    })
-    vim.diagnostic.show()
+    vim.diagnostic.enable()
   end
 end
 
