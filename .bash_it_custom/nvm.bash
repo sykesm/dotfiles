eeb1a26d6ea1d5833b911d345b8bd4e47f01457c
Matthew Sykes <matthew.sykes@gmail.com>
2023-03-06 19:25:20 -0500

    nvm: configure a brew installed nvm

--- .bash_it_custom/nvm.bash
+++ .bash_it_custom/nvm.bash
@@ -2,10 +2,17 @@
 # vi: set ft=sh:
 
 if [ -d "$HOME/.config/nvm" ]; then
-  export NVM_DIR="$HOME/.config/nvm"
+    export NVM_DIR="$HOME/.config/nvm"
 elif [ -d "$HOME/.nvm" ]; then
-  export NVM_DIR="$HOME/.nvm"
+    export NVM_DIR="$HOME/.nvm"
 fi
 
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-[ -s "$NVM_DIR/bash_completion" ] && source "$NVM_DIR/bash_completion"
+if [ -s "$NVM_DIR/nvm.sh" ]; then
+    source "$NVM_DIR/nvm.sh"
+elif [ -x "$(command -v brew)" ] && [ -s "$(brew --prefix nvm)/nvm.sh" ]; then
+    source "$(brew --prefix nvm)/nvm.sh"
+fi
+
+if [ -x "$(command -v brew)" ] && [ -s "$(brew --prefix nvm)/etc/bash_completion.d/nvm" ]; then
+    source "$(brew --prefix nvm)/etc/bash_completion.d/nvm"
+fi
--- .oh-my-zsh-custom/nvm.zsh
+++ .oh-my-zsh-custom/nvm.zsh
@@ -1,9 +1,17 @@
 # vi: set ft=zsh:
 
 if [ -d "$HOME/.config/nvm" ]; then
-  export NVM_DIR="$HOME/.config/nvm"
+    export NVM_DIR="$HOME/.config/nvm"
 elif [ -d "$HOME/.nvm" ]; then
-  export NVM_DIR="$HOME/.nvm"
+    export NVM_DIR="$HOME/.nvm"
 fi
 
-[ -s "$NVM_DIR/nvm.sh" ] && source "$NVM_DIR/nvm.sh"
+if [ -s "$NVM_DIR/nvm.sh" ]; then
+    source "$NVM_DIR/nvm.sh"
+elif [ -x "$(command -v brew)" ] && [ -s "$(brew --prefix nvm)/nvm.sh" ]; then
+    source "$(brew --prefix nvm)/nvm.sh"
+fi
+
+if [ -x "$(command -v brew)" ] && [ -s "$(brew --prefix nvm)/etc/bash_completion.d/nvm" ]; then
+    source "$(brew --prefix nvm)/etc/bash_completion.d/nvm"
+fi
