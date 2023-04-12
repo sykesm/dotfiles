65347102e98d8a1ee940faa825c16d0ff92a5527
Matthew Sykes <matthew.sykes@gmail.com>
2023-04-12 08:42:52 -0400

    Move luasnip setup call before cmp setup

--- .config/nvim/lua/sykesm/plugins/cmp.lua
+++ .config/nvim/lua/sykesm/plugins/cmp.lua
@@ -18,6 +18,8 @@ end
 
 require('luasnip.loaders.from_vscode').lazy_load()
 
+luasnip.config.setup({})
+
 local function has_words_before()
   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
   return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
@@ -58,8 +60,6 @@ local function cmp_mapping()
 
   return mapping
 end
-
-luasnip.config.setup({})
 
 cmp.setup({
   completion = {
