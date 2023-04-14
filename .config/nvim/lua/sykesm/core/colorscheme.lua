8c56f66f66a454ca2c8414ce5e0a9eefc5c56b49
Matthew Sykes <matthew.sykes@gmail.com>
2023-04-14 07:07:20 -0400

    nvim: add template for color scheme override

--- .config/nvim/lua/sykesm/core/colorscheme.lua
+++ .config/nvim/lua/sykesm/core/colorscheme.lua
@@ -12,3 +12,13 @@ if not status then
   print('Colorscheme not found!') -- print error if colorscheme not installed
   return
 end
+
+-- -- If/when overrides are necessary..
+-- local custom_highlight = vim.api.nvim_create_augroup("CustomHighlight", {})
+-- vim.api.nvim_create_autocmd("ColorScheme", {
+--   pattern = "nightfly",
+--   callback = function()
+--     vim.api.nvim_set_hl(0, "Function", { fg = "#82aaff", bold = true })
+--   end,
+--   group = custom_highlight,
+-- })
