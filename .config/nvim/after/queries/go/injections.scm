36cb8cd2e1f463b7f6264a80d6dcc88f12591773
Matthew Sykes <matthew.sykes@gmail.com>
2023-07-28 08:06:34 -0400

    nvim: injection for probable json in raw string
    go.nvim has injections for literals that are assigned to consts and vars
    of string literals with names that include [Jj]son but, in my
    experience, that's pretty uncommon.
    
    This adds an injection that highlights raw string literals that start
    with `{` or '[` as JSON. It's not perfect but will probably do what I
    want 99% of the time.

--- .config/nvim/after/queries/go/injections.scm
+++ .config/nvim/after/queries/go/injections.scm
@@ -9,3 +9,10 @@
  ] @graphql
  (#contains? @graphql "@genqlient")
  (#offset! @graphql 0 1 0 -1))
+
+; ----------------------------------------------------------------
+; probably json
+
+((raw_string_literal) @json
+ (#match? @json "^`(\\\{|\\\[)")
+ (#offset! @json 0 1 0 -1))
