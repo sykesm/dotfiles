04a14aac5a57533c758c2d083436429d38266f9a
Matthew Sykes <matthew.sykes@gmail.com>
2024-05-29 20:09:38 -0400

    nvim: tweak the nightfly FloatBorder color

--- .config/nvim/lua/sykesm/core/colorscheme.lua
+++ .config/nvim/lua/sykesm/core/colorscheme.lua
@@ -1,10 +1,20 @@
 -- colorscheme.lua
+-- nightfly is a 24-bit color scheme with LSP support
+
+local custom_highlight = vim.api.nvim_create_augroup('CustomHighlight', {})
+vim.api.nvim_create_autocmd('ColorScheme', {
+  pattern = 'nightfly',
+  callback = function()
+    -- Use CelloBlue instead of SlateBlue
+    vim.api.nvim_set_hl(0, 'FloatBorder', { link = 'NightflyCelloBlue' })
+  end,
+  group = custom_highlight,
+})
 
 -- nightfly color scheme options
 vim.g.nightflyTransparent = 1
 vim.g.nightflyUndercurls = 1
 
--- nightfly is a 24-bit color scheme with LSP support
 local status, _ = pcall(function()
   vim.cmd('colorscheme nightfly')
 end)
@@ -12,13 +22,3 @@ if not status then
   print('Colorscheme not found!') -- print error if colorscheme not installed
   return
 end
-
--- -- If/when overrides are necessary..
--- local custom_highlight = vim.api.nvim_create_augroup("CustomHighlight", {})
--- vim.api.nvim_create_autocmd("ColorScheme", {
---   pattern = "nightfly",
---   callback = function()
---     vim.api.nvim_set_hl(0, "Function", { fg = "#82aaff", bold = true })
---   end,
---   group = custom_highlight,
--- })
