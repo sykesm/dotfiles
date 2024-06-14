e960ecf96ed659d6ad40006dbe539ee6ad7605eb
Matthew Sykes <matthew.sykes@gmail.com>
2024-06-14 07:07:05 -0400

    nvim: lazy load vim-matchup

--- .config/nvim/lua/sykesm/plugins/vim-matchup.lua
+++ .config/nvim/lua/sykesm/plugins/vim-matchup.lua
@@ -2,6 +2,7 @@
 
 return {
   'andymass/vim-matchup', -- extended matching
+  event = 'BufReadPost',
   config = function()
     vim.g.matchup_matchparen_enabled = 1 --  override with :DoMatchParen,:NoMatchParen
     vim.g.matchup_matchparen_offscreen = { method = 'status' }
