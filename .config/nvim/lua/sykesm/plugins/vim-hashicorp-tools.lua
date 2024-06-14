a3036b3646d4e9447ba6c4b395ba83cc87f4f085
Matthew Sykes <matthew.sykes@gmail.com>
2024-06-14 09:55:14 -0400

    nvim: lazy load hashicorp plugin

--- .config/nvim/lua/sykesm/plugins/vim-hashicorp-tools.lua
+++ .config/nvim/lua/sykesm/plugins/vim-hashicorp-tools.lua
@@ -2,6 +2,20 @@
 
 return {
   'hashivim/vim-hashicorp-tools',
+  ft = {
+    'terraform',
+    'ruby', -- Vagrantfile
+  },
+  cmd = {
+    'Consul',
+    'Nomad',
+    'Otto',
+    'Packer',
+    'Terraform',
+    'TerraformFmt',
+    'Vagrant',
+    'Vault',
+  },
   config = function()
     vim.g.terraform_align = 1
     vim.g.terraform_fmt_on_save = 1
