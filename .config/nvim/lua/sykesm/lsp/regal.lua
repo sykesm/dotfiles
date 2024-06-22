78fd5e640633bedba19962ea3930a800bba65fd4
Matthew Sykes <matthew.sykes@gmail.com>
2024-06-22 09:17:15 -0400

    nvim: remove regal diagnosticProvider workaround
    Looks like newer versions work with nvim 0.10. This also adds
    '.manifest' as a root maker file for rego.

--- .config/nvim/lua/sykesm/lsp/regal.lua
+++ .config/nvim/lua/sykesm/lsp/regal.lua
@@ -3,17 +3,9 @@
 
 local M = {}
 
-function M.regal_on_attach(client, bufnr)
-  -- regal doesn't implement pull diagnostics but claims it does
-  -- this causes errors starting with nvim 0.10 due to
-  -- https://github.com/neovim/neovim/issues/22838
-  client.server_capabilities.diagnosticProvider = nil
-  require('sykesm.lsp.on-attach')(client, bufnr)
-end
-
 local function regal_path()
-  -- Look for a custom build before falling back to a
-  -- version that's on the path.
+  -- Look for a custom build before falling back to a version that's
+  -- on the path.
   local custom_paths = {
     vim.fn.expand('~/projects/regal/regal'),
     'regal',
@@ -35,12 +27,12 @@ function M.setup()
   end
 
   local util = require('lspconfig.util')
-  local root_pattern = util.root_pattern('.oparoot', '*.rego')
+  local root_pattern = util.root_pattern('.oparoot', '.manifest', '*.rego')
 
   require('lspconfig').regal.setup({
     cmd = { path, 'language-server' },
     capabilities = require('sykesm.lsp.capabilities').create(),
-    on_attach = M.regal_on_attach,
+    on_attach = require('sykesm.lsp.on-attach'),
     root_dir = function(fname)
       return root_pattern(fname) or util.find_git_ancestor(fname)
     end,
