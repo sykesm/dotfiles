ea94d7ebac1bdfab8fb616694e568ac341a7cc6a
Matthew Sykes <matthew.sykes@gmail.com>
2024-10-27 00:12:03 -0400

    nvim: NVIM_LSP_DEBUG sets debug level logs for LSP

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -6,3 +6,11 @@ require('sykesm.core.augroups')
 require('sykesm.core.clipboard')
 require('sykesm.core.highlight')
 require('sykesm.core.keymaps')
+
+if vim.env.NVIM_LSP_DEBUG then
+  vim.lsp.set_log_level('debug')
+  vim.print(require('sykesm.lsp.capabilities').create())
+  if vim.fn.has('nvim-0.5.1') == 1 then
+    require('vim.lsp.log').set_format_func(vim.inspect)
+  end
+end
