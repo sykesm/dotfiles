f53735a752982054d97f1d27378369c162a4e129
Matthew Sykes <matthew.sykes@gmail.com>
2024-10-27 00:03:06 -0400

    nvim: update nvim-cmp configuration

--- .config/nvim/lua/sykesm/plugins/cmp.lua
+++ .config/nvim/lua/sykesm/plugins/cmp.lua
@@ -30,15 +30,11 @@ function M.config()
 
   luasnip.config.setup({})
 
-  local function has_words_before()
-    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
-  end
-
   local function cmp_mapping()
+    local cmp_select = { behavior = cmp.SelectBehavior.Select }
     local mapping = {
-      ['<C-p>'] = cmp.mapping.select_prev_item(),
-      ['<C-n>'] = cmp.mapping.select_next_item(),
+      ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+      ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
       ['<C-b>'] = cmp.mapping.scroll_docs(-4),
       ['<C-f>'] = cmp.mapping.scroll_docs(4),
       ['<C-y>'] = cmp.mapping.confirm({ select = true }),
@@ -57,25 +53,29 @@ function M.config()
       end
     end, { 'i', 's' })
 
+    mapping['<CR>'] = cmp.mapping({
+      i = function(fallback)
+        if cmp.visible() and cmp.get_active_entry() then
+          cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
+        else
+          fallback()
+        end
+      end,
+      s = cmp.mapping.confirm({ select = true }),
+      c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
+    })
+
     mapping['<Tab>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
-      elseif has_words_before() then
-        cmp.complete()
+        cmp.select_next_item(cmp_select)
       else
-        -- The fallback function sends a already mapped key.
-        -- In this case, it's probably `<Tab>`.
         fallback()
       end
     end, { 'i', 's' })
 
     mapping['<S-Tab>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
+        cmp.select_prev_item(cmp_select)
       else
         fallback()
       end
@@ -87,6 +87,18 @@ function M.config()
   cmp.setup({
     completion = {
       completeopt = 'menu,menuone,noinsert,noselect',
+    },
+    confirmation = {
+      default_behavior = cmp.ConfirmBehavior.Insert,
+      get_commit_characters = function(commit_characters)
+        if vim.bo.ft == 'go' then
+          return vim.fn.extend({ '.', ',', '(', '[', ' ' }, commit_characters or {})
+        end
+        return commit_characters
+      end,
+    },
+    experimental = {
+      ghost_text = false,
     },
     mapping = cmp.mapping.preset.insert(cmp_mapping()),
     preselect = cmp.PreselectMode.None,
