3b0b8e70af0881c143d08a28d681ac5b2adca06d
Matthew Sykes <matthew.sykes@gmail.com>
2025-01-29 19:45:07 -0500

    nvim: update injection queries for go
    Looks like the parser has added {interpreted,raw}_string_literal_content
    as a child of {interpreted,raw}_string_literal. This broke injections.
    
    Looks like go.nvim tried to fix their injections but didn't remove the
    offset processing so SQL injections are missing.

--- .config/nvim/after/queries/go/injections.scm
+++ .config/nvim/after/queries/go/injections.scm
@@ -4,25 +4,22 @@
 ; a genqlient graph query
 
 ([
-   (interpreted_string_literal)
-   (raw_string_literal)
+   (interpreted_string_literal_content)
+   (raw_string_literal_content)
  ] @injection.content
  (#contains? @injection.content "@genqlient")
- (#offset! @injection.content 0 1 0 -1)
  (#set! injection.language "graphql"))
 
 ; ----------------------------------------------------------------
 ; probably json
 
-((raw_string_literal) @injection.content
- (#match? @injection.content "^`(\\\{|\\\[)")
- (#offset! @injection.content 0 1 0 -1)
+((raw_string_literal_content) @injection.content
+ (#match? @injection.content "^[\n|\t| ]*(\\\{|\\\[)")
  (#set! injection.language "json"))
 
 ; ----------------------------------------------------------------
 ; probably yaml
 
-((raw_string_literal) @injection.content
- (#match? @injection.content "^`---\n")
- (#offset! @injection.content 0 1 0 -1)
+((raw_string_literal_content) @injection.content
+ (#match? @injection.content "^[\n|\t| ]*---\n")
  (#set! injection.language "yaml"))
