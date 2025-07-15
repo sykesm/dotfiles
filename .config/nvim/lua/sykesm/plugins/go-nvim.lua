60ce46a935bf231cc4a15739b27870fd4d793519
Matthew Sykes <matthew.sykes@gmail.com>
2025-07-15 06:38:55 -0400

    nvim(go): pin before err format as comment
    The plugin author has decided error handling should be formatted as
    comments. 🤷‍♂️
    
    Pinning to the last commit before the change until I can figure out a
    better way to handle it.

--- .config/nvim/lua/sykesm/plugins/go-nvim.lua
+++ .config/nvim/lua/sykesm/plugins/go-nvim.lua
@@ -2,6 +2,8 @@
 
 return {
   'ray-x/go.nvim',
+  -- The last commit before errors were highlighted like comments.
+  commit = 'a3455f48cff718a86275115523dcc735535a13aa',
   dependencies = {
     'ray-x/guihua.lua', -- float term, go.nvim gui support
   },
