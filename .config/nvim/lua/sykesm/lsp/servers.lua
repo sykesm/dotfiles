706b1a4e248e3b98c46baf097acf21731d94707b
Matthew Sykes <matthew.sykes@gmail.com>
2025-09-20 09:44:10 -0400

    nvim: switch from terraformls to tofu_ls

--- .config/nvim/lua/sykesm/lsp/servers.lua
+++ .config/nvim/lua/sykesm/lsp/servers.lua
@@ -102,7 +102,7 @@ local server_configs = {
     },
   },
   tailwindcss = {},
-  terraformls = {},
+  tofu_ls = {},
   ts_ls = function(_)
     -- Empty function used to ensure that the typescript server is
     -- installed but the setup is done by typescript-tools.
